a2747fefcae60392f19dd8cb768e3e8598d90e56
Andrew Morrow <andrew.d.morrow@gmail.com>
2014-03-01 15:57:55 -0700

    Implement gps task threading/sync

--- gps.rs
+++ gps.rs
@@ -1,8 +1,10 @@
 // gps.rs
 // Uses a Unix socket to connect to gpsd.
 
-use extra::arc::RWArc;
+extern mod extra;
+use self::extra::arc::RWArc;
 
+#[deriving(Clone)]
 pub struct Fix {
 	time: f64,
 	lat: f64,
@@ -12,22 +14,40 @@ pub struct Fix {
 
 pub struct Client {
 	priv fix: RWArc<Option<~Fix>>,
+	priv bg_chan: Chan<bool>
 }
 
 impl Client {
 	pub fn new() -> Client {
-		let client = Client { fix: RWArc.new(None) };
+		let (port, chan) = Chan::new();
+		let client = Client { fix: RWArc::new(None), bg_chan: chan };
 		let background_storage = client.fix.clone();
-		do spawn {
-			gpsd_listener(background_storage);
-		};
+		spawn(proc() { gpsd_listener(background_storage, port); });
 		client
 	}
-	// TODO add closure-based reading of fix
-	// TODO implement destructor including halting background task
+
+	pub fn read(&self) -> Option<~Fix> {
+		self.fix.read(|data: &Option<~Fix>| -> Option<~Fix> {
+			data.clone()
+		})
+	}
 }
 
-fn gpsd_listener(storage: RWArc<Option<Fix>>) {
+impl Drop for Client {
+	fn drop(&mut self) {
+		self.bg_chan.try_send(true);
+	}
+}
+
+fn gpsd_listener(storage: RWArc<Option<~Fix>>, halt_port: Port<bool>) {
 	// TODO open Unix socket to gpsd and read until termination message from foreground
+	loop {
+		//...program code here
+
+		match halt_port.try_recv() {
+			Some(stop) if stop => break,
+			_ => continue
+		};
+	}
 }
 
--- main.rs
+++ main.rs
@@ -1,13 +1,10 @@
 mod gps;
 
 fn main() {
-	let dist = unsafe { gps::earth_distance(55.594978, 2.718338, 55.594985, 2.718341) };
-	println!("Distance: {}", dist);
-
 	let client = gps::Client::new();
 
 	match client.read() {
-		Some(fix) => println!("Got a fix at {}, {}", fix.latitude, fix.longitude),
+		Some(real_fix) => println!("Got a fix at {}, {}", real_fix.lat, real_fix.lon),
 		None => println!("Couldn't get a fix")
 	};
 }

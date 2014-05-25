ea6e6cf515e84495edc0a6a7a6efe94ecae04001
Andrew Morrow <andrew.d.morrow@gmail.com>
2014-05-25 16:48:24 -0600

    (Broken) Begin skeleton STN1110 interface

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
-import ("fmt"; "time"; "github.com/morrowa/tahoe-pi/gps")
+import ("fmt"; "time"; "github.com/morrowa/tahoe-pi/gps"; "github.com/morrowa/tahoe-pi/stn1110")
 
 func main() {
+
+	_ := stn1110.Connect("/dev/null")
 
 	reports := gps.Connect("127.0.0.1:2947")
 
--- stn1110/stn1110.go
+++ stn1110/stn1110.go
@@ -1,3 +1,23 @@
 package stn1110
 
+type STNCommandMode string
+
+const (
+	AT  STNCommandMode = "AT"
+	ST  STNCommandMode = "ST"
+	OBD STNCommandMode = ""
+)
+
+type STNCommand struct {
+	Mode STNCommandMode
+	//...
+}
+
+type STNResponse struct {
+	//...
+}
+
+func Connect(devPath string) (commandChan chan<- STNCommand, responseChan <-chan STNResponse) {
+	//...
+}
 
